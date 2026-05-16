2ece70732d152cd1094ea3330e16e315ac13d93d
Zishan Sheikh <zishanyt241@gmail.com>
2026-05-16 12:38:40 +0530

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -1,64 +1,9 @@
 metadata_fields = [
 
-    # Basic
-
     "title",
 
     "artist",
 
-    "author",
-
-    "album",
-
-    "genre",
-
-    "language",
-
-    "year",
-
-
-    # Advanced
-
-    "publisher",
-
-    "comment",
-
-    "description",
-
-    "composer",
-
-    "encoder",
-
-    "director",
-
-    "producer",
-
-    "lyrics",
-
-    "subtitle",
-
-    "copyright",
-
-
-    # OTT Style
-
-    "series_name",
-
-    "season",
-
-    "episode",
-
-    "network",
-
-    "imdb",
-
-
-    # Extra
-
-    "website",
-
-    "rating",
-
-    "encoded_by"
+    "year"
 
 ]
