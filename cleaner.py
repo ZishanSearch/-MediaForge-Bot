5f367f3d1adea37594136c2b3ff99893c147d32a
Zishan Sheikh <zishanyt241@gmail.com>
2026-05-17 17:04:41 +0530

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -4,73 +4,29 @@ import shutil
 
 
 
-async def delete_files(*files):
+async def delete_files(
 
+    *paths
 
-    for file in files:
+):
 
+    for path in paths:
 
         try:
 
-            if not file:
+            if not path:
+
                 continue
 
 
             # File Delete
 
-            if os.path.isfile(file):
-
-                os.remove(file)
-
-
-            # Folder Delete
-
-            elif os.path.isdir(file):
-
-                shutil.rmtree(
-
-                    file,
-
-                    ignore_errors=True
-
-                )
-
-        except:
-            pass
-
-
-
-async def clean_temp_folder():
-
-    temp_folder = "temp"
-
-
-    if not os.path.exists(
-        temp_folder
-    ):
-
-        return
-
-
-    for item in os.listdir(
-        temp_folder
-    ):
-
-        path = os.path.join(
-
-            temp_folder,
-
-            item
-
-        )
-
-
-        try:
-
             if os.path.isfile(path):
 
                 os.remove(path)
 
+
+            # Folder Delete
 
             elif os.path.isdir(path):
 
@@ -83,4 +39,5 @@ async def clean_temp_folder():
                 )
 
         except:
+
             pass
